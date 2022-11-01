30ecf0a417dec25414e230ade38926c8b137085c
Welyson Rosa <welysonsr@gmail.com>
2022-11-01 16:43:14 -0300

    feat: rota login desenvolvida

--- api/app.js
+++ api/app.js
@@ -1,10 +1,13 @@
 require('express-async-errors');
 const express = require('express');
 const error = require('../middlewares/error');
+const user = require('../routes/user')
 
 const app = express();
 
 app.use(express.json());
+
+app.use('/user', user)
 
 app.use(error);
 
---
+++ services/user.js
@@ -0,0 +1,42 @@
+const { User } = require('../database/models');
+const md5 = require('md5');
+
+const regaxEmail = /\S+@\S+\.\S+/;
+const regaxPassword = /^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{8,20}$/;
+
+const register = async ({ firstName, lestName, email, password, role }) => {
+  const userExist = await User.findOne({ where: { email } });
+  if (userExist) throw new Error('406|Email já cadastrado!');
+  
+  if(!firstName || !lestName || !email || !password || !role) {
+    throw new Error('406|Campo obrigatório!');
+  }
+
+  if (firstName.length < 3 || lestName.length < 4) {
+    throw new Error('406|Formato de nome incorreto!');
+  }
+
+  if (!regaxEmail.test(email)) {
+    throw new Error('406|Formato de e-mail incorreto!');
+  }
+
+  if (!regaxPassword.test(password)) {
+    throw new Error('406|Formato de senha incorreto!');
+  }
+
+  const newUser = {
+    firstName,
+    lestName,
+    email,
+    password: md5(password),
+    role
+  };
+
+  const { dataValues } = await User.create(newUser);
+
+  if (dataValues.id) return { code: 200, response: 'Usuário criado com sucesso!'};
+};
+
+module.exports = {
+  register
+}
